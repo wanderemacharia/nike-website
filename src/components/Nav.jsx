c92d462469aba93d9765a449862ecf13e2764650
wanderemacharia <wandere07@gmail.com>
2023-11-08 18:34:27 +0300

    'added a hero section'

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,6 +24,14 @@ const Nav = () => {
             </li>
           ))}
         </ul>
+        <div className='hidden max-lg:block'>
+          <img 
+          src={hamburger}
+          alt='hamburger'
+          width={25}
+          height={25}>
+          </img>
+        </div>
       </nav>
     </header>
   );
