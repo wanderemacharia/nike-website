6d0036ae0f45fe146525a04eff57ef237cfbf080
wanderemacharia <wandere07@gmail.com>
2023-11-09 23:57:39 +0300

    added statistics to hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,13 @@
 import { arrowRight } from '../assets/icons';
 import { Button } from '../components/Button';
+import { statistics } from '../constants/index';
 
 
 const Hero = () => {
   return (
     <section 
     id="home"
-    className="w-full flex xl:flex-row flex col justify-center min-h-screen gap-10 max-container">
+    className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div
       className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p>Our Summer Collection</p>
@@ -18,6 +19,15 @@ const Hero = () => {
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for you active lifestyle.</p>
         <Button label='Shop Now' iconURL={arrowRight}/>
+
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
